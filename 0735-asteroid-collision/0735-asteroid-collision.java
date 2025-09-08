9874befc6feda8968f0bc5c0155fd141101ed8a7
Yugyeong Na <zz6cod@gmail.com>
2025-09-08 21:33:11 +0900

    Time: 3 ms (94.61%), Space: 45.55 MB (34.83%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,39 +1,30 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for (int asteroid : asteroids) {
-            // If the last element in the stack is negative, there's no need to check for collisions.
-            // This is because both rocks are moving left, so they can't explode.
-            // That's why we only check for collisions when the top of the stack is positive.
+            boolean destroyed = false;
 
-            // 1. Remove
-            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
-                // Since we know which is negative, no need to use 'Math.abs()'.
-                if (stack.peek() < -asteroid) {
-                    stack.pop();
-                    // Keep comparing
+            while (!stack.isEmpty() && stack.peekLast() > 0 && asteroid < 0) {
+                if (stack.peekLast() < -asteroid) {
+                    stack.pollLast();
                     continue;
+                } else if (stack.peekLast() == -asteroid) {
+                    stack.pollLast();
                 }
-                else if (stack.peek() == -asteroid) {
-                    stack.pop();
-                    // Both exploded. (Terminate the loop and reset asteroid)
-                    asteroid = 0;
-                }
+                destroyed = true;
                 break;
             }
 
-            // 2. Add (After all the smaller, exploded asteroids had been removed)
-            if (stack.isEmpty() || stack.peek() < 0 || asteroid > 0) {
-                if (asteroid != 0) {
-                    stack.push(asteroid);
-                }
+            if (!destroyed) {
+                stack.addLast(asteroid);
             }
         }
 
         int[] result = new int[stack.size()];
-        for (int i = 0; i < stack.size(); i++) {
-            result[i] = stack.get(i);
+        int i = 0;
+        for (int val : stack) {
+            result[i++] = val;
         }
         return result;
     }
