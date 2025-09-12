f326e4162249c6b2176acba80966e8e10db52baa
Yugyeong Na <zz6cod@gmail.com>
2025-09-12 23:41:47 +0900

    Time: 2 ms (87.14%), Space: 55.3 MB (75.23%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        
+        // 왼쪽 곱 저장
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+        
+        // 오른쪽 곱과 곱해주기
+        int R = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * R;
+            R *= nums[i];
+        }
+        
+        return answer;
+    }
+}
