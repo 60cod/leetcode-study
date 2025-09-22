18a6c918c8ecc61c4be080a7d775405d09d90519
Yugyeong Na <zz6cod@gmail.com>
2025-09-22 15:37:48 +0900

    Time: 21 ms (60.77%), Space: 54.3 MB (47.81%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,12 +1,20 @@
 class RecentCounter {
-    Queue<Integer> queue;
-    public RecentCounter() {
-        queue = new LinkedList();
-    }
 
-    public int ping(int t) {
-        queue.add(t);
-        while (queue.peek() < t - 3000) queue.poll();
-        return queue.size();
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
     }
+    
+    public int ping(int t) {
+        queue.add(t); // 새 요청 추가
+
+        // 3000ms 이전 요청 제거
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size(); // 남은 요청 수 반환
+    }
+    // Time complexity: O(n)
 }
