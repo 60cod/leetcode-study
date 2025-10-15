8c8772fe6835f2b6bddd09292d39653ad0a12975
Yugyeong Na <zz6cod@gmail.com>
2025-10-15 11:55:05 +0900

    Time: 0 ms (100%), Space: 41.5 MB (94.05%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,18 +1,27 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         int i = 0, j = 0;
-        int n = word1.length(), m = word2.length();
+        int n1 = word1.length(), n2 = word2.length();
 
-        while (i < n && j < m) {
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
+        // 교차로 문자 추가
+        while (i < n1 && j < n2) {
+            result.append(word1.charAt(i++));
+            result.append(word2.charAt(j++));
         }
 
-        // 남은 부분 추가
-        if (i < n) sb.append(word1.substring(i));
-        if (j < m) sb.append(word2.substring(j));
+        // 남은 문자 처리
+        if (i < n1) result.append(word1.substring(i));
+        if (j < n2) result.append(word2.substring(j));
 
-        return sb.toString();
+        return result.toString();
     }
 }
+
+/*
+Time Complexity: O(n1 + n2)
+ - 두 문자열을 한 번씩 순회
+
+Space Complexity: O(n1 + n2)
+ - 결과 문자열 저장용 StringBuilder 사용
+*/
