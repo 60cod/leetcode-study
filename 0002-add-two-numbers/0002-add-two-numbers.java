2134927bbb7c6c9c126ec996725ce86500476265
Yugyeong Na <zz6cod@gmail.com>
2026-02-08 16:12:49 +0900

    Time: 1 ms (99.69%), Space: 46.5 MB (53.9%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0); // 결과를 위한 더미 노드
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+    // 시간 복잡도: O(max(m, n)) - m, n은 각 리스트 길이
+    // 공간 복잡도: O(max(m, n)) - 새 리스트 노드 생성
+
+}
