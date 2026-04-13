ff2121d9454c3eb304cb6cc421939fb0f48237b8
Yugyeong Na <zz6cod@gmail.com>
2026-04-14 00:08:28 +0900

    Time: 0 ms (100%), Space: 43.7 MB (84.97%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // 왼쪽 정렬 구간
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // 오른쪽 정렬 구간
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+// 시간 복잡도 - O(log n), n은 배열 nums의 길이
+// 공간 복잡도 - O(1)
