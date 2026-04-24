b0ba2ec132340e0650a7b2717220338025c6ce53
Yugyeong Na <zz6cod@gmail.com>
2026-04-24 15:43:35 +0900

    Time: 0 ms (100%), Space: 44.5 MB (83.77%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
+
+// 시간 복잡도 - O(log n), n은 배열 nums의 길이
+// 공간 복잡도 - O(1)
