57a790d737100b2cb57e393cca0280ef9dc92d8e
Yugyeong Na <zz6cod@gmail.com>
2026-05-19 14:23:05 +0900

    Time: 30 ms (52.26%), Space: 47.4 MB (57.37%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length(), m = p.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                char pc = p.charAt(j - 1);
+
+                if (pc == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else if (pc == '?' || pc == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
+
+// 시간 복잡도 - O(n * m), n은 문자열 s 길이, m은 패턴 p 길이
+// 공간 복잡도 - O(n * m)
