c0f56bc166d57bfd75eb5f57d4d2c842dbdd5686
Yugyeong Na <zz6cod@gmail.com>
2026-06-03 13:35:38 +0900

    Time: 1 ms (99.95%), Space: 77.2 MB (47.36%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
+
+// 시간 복잡도 - O(n), n은 배열 nums의 길이
+// 공간 복잡도 - O(1)
