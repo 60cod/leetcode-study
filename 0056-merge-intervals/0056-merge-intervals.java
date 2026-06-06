cb50c4eea9b972fea19d90e35148c8fff8dc219c
Yugyeong Na <zz6cod@gmail.com>
2026-06-07 05:29:33 +0900

    Time: 8 ms (90.64%), Space: 49.4 MB (13.52%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (result.isEmpty() || result.get(result.size() - 1)[1] < interval[0]) {
+                result.add(interval);
+            } else {
+                result.get(result.size() - 1)[1] =
+                    Math.max(result.get(result.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+// 시간 복잡도 - O(n log n), n은 intervals의 길이 (정렬 때문)
+// 공간 복잡도 - O(n)
