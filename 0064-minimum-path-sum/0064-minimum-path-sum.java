02114c3d44fdd0478c81a6b333757a33d94e6d98
Yugyeong Na <zz6cod@gmail.com>
2026-07-12 00:13:18 +0900

    Time: 3 ms (73.78%), Space: 50.4 MB (37.54%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] dp = new int[n];
+
+        dp[0] = grid[0][0];
+
+        for (int j = 1; j < n; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
+
+// 시간 복잡도 - O(m * n), m은 행의 개수, n은 열의 개수
+// 공간 복잡도 - O(n)
